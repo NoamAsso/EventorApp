e30f40f547dcd503721bb28aa8f31b315da059cb
NoamAsso <assoulinenoam@gmail.com>
2018-01-23 15:11:44 +0200

    v1.11

--- Eventor/app/src/main/java/com/example/noam/eventor/EventPage.java
+++ Eventor/app/src/main/java/com/example/noam/eventor/EventPage.java
@@ -1,8 +1,13 @@
 package com.example.noam.eventor;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.MapView;
 
@@ -13,6 +18,32 @@ public class EventPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_page);
         Intent intent = getIntent();
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_event_page);
+// The View with the BottomSheetBehavior
+        View bottomSheet = coordinatorLayout.findViewById(R.id.bottom_sheet);
+        final BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                // React to state change
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheet.setVisibility(View.VISIBLE);
+                } else {
+                    bottomSheet.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                // React to dragging events
+            }
+        });
+
+    behavior.setHideable(false);
+        behavior.setPeekHeight(300);
     }
 }
