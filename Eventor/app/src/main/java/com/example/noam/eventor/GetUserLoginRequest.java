2892053b487ca70603ae437573dca4abc3a673ff
itayrin <itay.ringler@amdocs.com>
2018-01-25 00:25:33 +0200

    v1.34

--- Eventor/app/src/main/java/com/example/noam/eventor/GetUserLoginRequest.java
+++ Eventor/app/src/main/java/com/example/noam/eventor/GetUserLoginRequest.java
@@ -3,12 +3,12 @@ package com.example.noam.eventor;
 import static com.example.noam.eventor.NetworkManager.ReqMethod.GET;
 
 /**
- * Created by itayrin on 1/22/2018.
+ * Created by itayrin on 1/25/2018.
  */
 
-public class GetEventsRequest extends BaseRequest {
+public class GetUserLoginRequest extends BaseRequest {
 
-    public GetEventsRequest(ServerCallback mCallback) {
+    public GetUserLoginRequest(ServerCallback mCallback) {
         super(mCallback);
     }
 
@@ -19,7 +19,6 @@ public class GetEventsRequest extends BaseRequest {
 
     @Override
     public String getServiceUrl() {
-        return "events";
+        return "login";
     }
-
 }
